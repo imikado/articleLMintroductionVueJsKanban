35f1bb20c56cd5e9e0f610f2b624d115fd516c89
mika <imikado@gmail.com>
2020-02-16 15:43:01 +0100

    install

--- js/composants/kanbanCol.js
+++ js/composants/kanbanCol.js
@@ -2,61 +2,21 @@ Vue.component('kanban-col', {
     data:function(){
         return {
             
- 
-            dragEnterStatus:false,
         }
     },
 
-    created:function(){
-         
-    },
     methods: {
-
-
-        load:function(){
-            this.list=modelTask.findListByStatus(this.status);
-
-            console.log(this.list);
-        },
-
-        dragStart:function(id_,evt_){
-            myApp.enableTaskMoving(id_);
-        },
-
-        dragEnd:function(){
-            myApp.disableTaskMoving();
-        },
-
-        dragEnter:function(){
-            this.dragEnterStatus=true;
-        },
-        dragExit:function(){
-            this.dragEnterStatus=false;
-        },
-
-        dragFinish:function(id_,evt_){
-            myApp.updateStatusTaskById(myApp.getTaskMovingId(),this.status);
-
-            myApp.disableTaskMoving();
-        },
-
-        isTaskMoving:function(){
-            if(this.taskmovingid > -1){
-                return true;
-            }
-            return false;
-        },
 
         getClass:function(){
             return 'col-sm-'+this.cols;
         }
     },
-    props:['title','cols','cols','status','list','taskmovingid'],
+    props:['title','cols','list'],
     template: `
     <div  :class="getClass()" >
         <h2 class="text-success" >{{title}}</h2>
 
-        <div v-for="task in list" v-if="task.status==status" class="card kanbanTask" @dragstart="dragStart(task.id, $event)"  @dragend="dragEnd()" draggable="true" >
+        <div v-for="task in list"  class="card kanbanTask" >
 
             
             <h5 class="card-header bg-default text-dark movable"  >{{ task.title}}</h5>
@@ -68,7 +28,7 @@ Vue.component('kanban-col', {
             </div>
         </div>
 
-        <div class="card kanbanTaskDrop"   v-if="isTaskMoving()"     @drop="dragFinish( $event)" ondragover="window.allowDrop(event)">
+        <div class="card kanbanTaskDrop" >
         <h5 class="card-body ">&nbsp;</h5>
         </div>
 
